c471740dadb5556b1aaf493d4574c36f60063eff
Evoni Feather <EvoniFeather@gmail.com>
2022-10-11 21:49:13 -0600

    part9.2

--- src/app/components/game-details/game-details.component.ts
+++ src/app/components/game-details/game-details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap, tap } from 'rxjs';
+import { Game } from 'src/app/interfaces/game';
+import { GameService } from 'src/app/services/game.service';
 
 @Component({
   selector: 'app-game-details',
@@ -6,10 +10,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game-details.component.scss']
 })
 export class GameDetailsComponent implements OnInit {
+  game: Game;
 
-  constructor() { }
+  constructor(
+    private gameService: GameService,
+    private route: ActivatedRoute,
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+        switchMap(params => {
+            return this.gameService.getById(params['gameId'])
+        }),
+        tap(game => this.game = game[0])
+    ).subscribe();
   }
 
 }
